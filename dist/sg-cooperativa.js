47a4359f4e2cedeb9c3b0e9d61bd1e84ff0a914a
Carlos <carlosthe19916@gmail.com>
2015-05-05 12:11:27 -0500

    add SGCaja

--- dist/sg-cooperativa.js
+++ dist/sg-cooperativa.js
@@ -33,7 +33,72 @@
     module.factory('SGBoveda', ['CooperativaRestangular',  function(CooperativaRestangular) {
 
         var url = 'bovedas';
-        var urlCount = 'countryCodes/count';
+        var urlCount = 'bovedas/count';
+
+        var modelMethos = {
+            $new: function(id){
+                return angular.extend({id: id}, modelMethos);
+            },
+            $build: function(){
+                return angular.extend({id: undefined}, modelMethos, {$save: function(){
+                    return CooperativaRestangular.all(url).post(this);
+                }});
+            },
+            $save: function() {
+                return CooperativaRestangular.one(url, this.id).customPUT(CooperativaRestangular.copy(this),'',{},{});
+            },
+
+            $find: function(id){
+                return CooperativaRestangular.one(url, id).get();
+            },
+            $search: function(queryParams){
+                return CooperativaRestangular.all(url).getList(queryParams);
+            },
+
+            $count: function(){
+                return CooperativaRestangular.one(urlCount).get();
+            },
+
+            $disable: function(){
+                return CooperativaRestangular.all(url+'/'+this.id+'/disable').post();
+            },
+            $remove: function(id){
+                return CooperativaRestangular.one(url, id).remove();
+            },
+
+            $abrir: function(denominaciones){
+                return CooperativaRestangular.all(url+'/'+this.id+'/abrir').post(denominaciones);
+            },
+            $cerrar: function(){
+                return CooperativaRestangular.all(url+'/'+this.id+'/cerrar').post();
+            },
+            $congelar: function(){
+                return CooperativaRestangular.all(url+'/'+this.id+'/congelar').post();
+            },
+            $descongelar: function(){
+                return CooperativaRestangular.all(url+'/'+this.id+'/descongelar').post();
+            },
+            $getDetalle: function(){
+                return CooperativaRestangular.all(url+'/'+this.id+'/detalle').getList();
+            }
+        };
+
+        CooperativaRestangular.extendModel(url, function(obj) {
+            if(angular.isObject(obj)) {
+                return angular.extend(obj, modelMethos);
+            } else {
+                return angular.extend({id: obj}, modelMethos)
+            }
+        });
+
+        return modelMethos;
+
+    }]);
+
+    module.factory('SGCaja', ['CooperativaRestangular',  function(CooperativaRestangular) {
+
+        var url = 'cajas';
+        var urlCount = 'cajas/count';
 
         var modelMethos = {
             $new: function(id){
@@ -78,8 +143,8 @@
             $descongelar: function(){
                 return CooperativaRestangular.all(url+'/'+this.id+'/descongelar').post();
             },
-            $getDetalle: function(){
-                return CooperativaRestangular.all(url+'/'+this.id+'/detalle').getList();
+            $getDetalle: function(queryParams){
+                return CooperativaRestangular.all(url+'/'+this.id+'/detalle').getList(queryParams);
             }
         };
 
